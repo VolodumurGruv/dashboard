bb3d3cb1b54fc32d1afb1012f060a4b4f78afacc
VolodumurGruv <gru.v@i.ua>
2022-03-23 09:34:58 +0200

    keep working

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  public tog = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggle() {
+    this.tog = !this.tog;
+    console.log(this.tog);
   }
-
 }
